d73a7429a81707b7986a9f2f85484b58a20c1b14
Luis José Villalobos <villalobos23@users.noreply.github.com>
2022-07-17 22:44:48 +0000

    removed the powered by header

--- myApp.js
+++ myApp.js
@@ -2,52 +2,7 @@ const express = require('express');
 const helmet = require("helmet");
 const app = express();
 app.use(helmet());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet.hidePoweredBy());
 
 module.exports = app;
 const api = require('./server.js');
